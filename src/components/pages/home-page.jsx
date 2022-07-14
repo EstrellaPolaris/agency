c0107d0af3c5f1de21ac7c4cc8ba990eb42c7f52
EstrellaPolaris <mshcherbinina73@gmail.com>
2022-07-14 13:13:28 +0200

    first commit

--- src/components/pages/home-page.jsx
+++ src/components/pages/home-page.jsx
@@ -1,11 +1,13 @@
 import { Intro } from "../intro/intro";
 import { Info } from "../info/info";
+import { Solutions } from "../solutions/solutions";
 
 const HomePage = () => {
     return (
         <>
         <Intro />
         <Info />
+        <Solutions />
         
         </>
     );
