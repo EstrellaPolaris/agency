7c7baeb2221b3686b8e98a95b63851a740c93c55
EstrellaPolaris <mshcherbinina73@gmail.com>
2022-07-15 12:48:26 +0200

    first commit

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import classes from './intro.module.scss';
+import Img from './img.jpg';
 
 const textAnimationright = {
   hidden: {
@@ -43,29 +44,25 @@ const Intro = () => {
       initial="hidden"
       whileInView="visible"
       viewport={{ amount: 0.1, once: true}}
-      className={`${classes.hero} container-fluid g-0`}
+      className={`${classes.intro} container-fluid g-0`}
     >
-     
-
-      <article className={classes.container__hero__title__left} >
+      <article className={classes.container__intro__title__left} >
         <motion.div custom={1} variants={textAnimationright} className={classes.fluid_font_size}>Technology</motion.div>
         <motion.div custom={1} variants={textAnimationright} className={classes.fluid_font_size}> +</motion.div>
       </article>
       
-      <article className={classes.container__hero__news}>
-        <motion.div custom={2} variants={textAnimationup} className={classes.container__hero__img}>
+      <article className={classes.container__intro__news}>
+        <motion.div custom={2} variants={textAnimationup} className={classes.container__intro__img}>
+          <img src={Img} alt="hero" className={classes.img} />
           <div className={classes.blog}>On the blog</div>
           <div className={classes.blog__title}>Luxury e-Commerce</div>
         </motion.div>
       </article>
-      <article className={classes.container__hero__title__right} >
+      <article className={classes.container__intro__title__right} >
         <motion.div custom={1} variants={textAnimationleft} className={`${classes.fluid_font_size} blue`}>Creativity</motion.div>
       </article>
-        
-      
-
-      <article className={classes.container__hero__usp}>
-        <motion.div custom={2} variants={textAnimationup}className={`${classes.container__hero__roundbtn}`}>
+      <article className={classes.container__intro__usp}>
+        <motion.div custom={2} variants={textAnimationup}className={`${classes.container__intro__roundbtn}`}>
           <div>&#9660;</div>
         </motion.div>
         <motion.h1 custom={2} variants={textAnimationup} className={classes.title__fh}>Get the edge with innovative product development, high-end web design, and effective development.</motion.h1>
